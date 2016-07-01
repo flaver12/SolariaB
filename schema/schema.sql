22fbf3d38b1b5731d0361af4a23b982eebee7c90
flaver12 <flaverkleiber@yahoo.de>
2016-07-01 13:19:21 +0200

    Added some missing files

--- schema/schema.sql
+++ schema/schema.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Erstellungszeit: 05. Nov 2014 um 00:47
+-- Erstellungszeit: 01. Jul 2016 um 12:37
 -- Server Version: 5.5.38
 -- PHP-Version: 5.4.34
 
@@ -11,98 +11,140 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Datenbank: `devstorm`
+-- Datenbank: `stormform`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `Category`
+-- Tabellenstruktur für Tabelle `config`
 --
 
-CREATE TABLE `Category` (
-`id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `Post`
---
-
-CREATE TABLE `Post` (
-`id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL,
-  `thread_id` int(11) DEFAULT NULL,
-  `replay` int(11) DEFAULT NULL,
-  `title` varchar(255) NOT NULL,
-  `body` text NOT NULL,
-  `created` int(11) NOT NULL,
-  `modified` int(11) DEFAULT '0',
-  `deleted` int(11) DEFAULT '0',
-  `hidden` int(11) DEFAULT '0',
-  `visible` int(11) DEFAULT '1'
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `Thread`
---
-
-CREATE TABLE `Thread` (
+CREATE TABLE `config` (
 `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `onlyAdmin` int(11) DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  `value` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `config`
+--
+
+INSERT INTO `config` (`id`, `name`, `value`) VALUES
+(1, 'site_name', 'Test');
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `User`
+-- Tabellenstruktur für Tabelle `navigation`
 --
 
-CREATE TABLE `User` (
+CREATE TABLE `navigation` (
 `id` int(11) NOT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `created` int(18) DEFAULT NULL,
-  `last_time_online` int(18) DEFAULT NULL,
-  `validated` int(11) DEFAULT '0',
-  `banned` varchar(10) DEFAULT 'n',
-  `admin` int(11) DEFAULT '0',
-  `online` int(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  `name` varchar(40) NOT NULL,
+  `link` varchar(100) NOT NULL,
+  `parent` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `navigation`
+--
+
+INSERT INTO `navigation` (`id`, `name`, `link`, `parent`) VALUES
+(1, 'Home', '/', NULL),
+(2, 'Home', '/', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `resource`
+--
+
+CREATE TABLE `resource` (
+`id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `resource` varchar(255) NOT NULL COMMENT 'If you have more than once, separet with a comma'
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `resource`
+--
+
+INSERT INTO `resource` (`id`, `name`, `resource`) VALUES
+(1, 'IndexController', 'index');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `role`
+--
+
+CREATE TABLE `role` (
+`id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `extends` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `role`
+--
+
+INSERT INTO `role` (`id`, `name`, `extends`) VALUES
+(1, 'Guest', NULL),
+(2, 'User', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user`
+--
+
+CREATE TABLE `user` (
+`id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `email`) VALUES
+(2, 'flaver', 'df32f14dafa7d09f5fe373fe3a1923277d83342e', 'flaverkleiber@yahoo.de');
 
 --
 -- Indizes der exportierten Tabellen
 --
 
 --
--- Indizes für die Tabelle `Category`
+-- Indizes für die Tabelle `config`
 --
-ALTER TABLE `Category`
+ALTER TABLE `config`
  ADD PRIMARY KEY (`id`);
 
 --
--- Indizes für die Tabelle `Post`
+-- Indizes für die Tabelle `navigation`
 --
-ALTER TABLE `Post`
+ALTER TABLE `navigation`
  ADD PRIMARY KEY (`id`);
 
 --
--- Indizes für die Tabelle `Thread`
+-- Indizes für die Tabelle `resource`
 --
-ALTER TABLE `Thread`
+ALTER TABLE `resource`
  ADD PRIMARY KEY (`id`);
 
 --
--- Indizes für die Tabelle `User`
+-- Indizes für die Tabelle `role`
 --
-ALTER TABLE `User`
+ALTER TABLE `role`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `user`
+--
+ALTER TABLE `user`
  ADD PRIMARY KEY (`id`);
 
 --
@@ -110,22 +152,27 @@ ALTER TABLE `User`
 --
 
 --
--- AUTO_INCREMENT für Tabelle `Category`
+-- AUTO_INCREMENT für Tabelle `config`
 --
-ALTER TABLE `Category`
+ALTER TABLE `config`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT für Tabelle `navigation`
+--
+ALTER TABLE `navigation`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT für Tabelle `Post`
+-- AUTO_INCREMENT für Tabelle `resource`
 --
-ALTER TABLE `Post`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+ALTER TABLE `resource`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT für Tabelle `Thread`
+-- AUTO_INCREMENT für Tabelle `role`
 --
-ALTER TABLE `Thread`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+ALTER TABLE `role`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT für Tabelle `User`
+-- AUTO_INCREMENT für Tabelle `user`
 --
-ALTER TABLE `User`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+ALTER TABLE `user`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
